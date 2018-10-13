bc7ed680f75ad5a42fddcac41272e92426e1f8b5
Brighton Dube <BrightonDube@users.noreply.github.com>
2018-10-13 09:07:12 +0200

    updated some filenames

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -23,7 +23,7 @@ namespace Checkpoints {
             const uint256& hash = i.second;
             BlockMap::const_iterator t = mapBlockIndex.find(hash);
             if (t != mapBlockIndex.end())
-                //return t->second;
+                return t->second;
         }
         return nullptr;
     }
